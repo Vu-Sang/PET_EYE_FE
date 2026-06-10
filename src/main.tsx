78ed6f5c2112b3a9bc2e9255aecc63c0a9413518
Dev43 <vudinhsang0403@gmail.com>
2026-06-10 13:58:01 +0700

    thaydoi

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.tsx';
 import './index.css';
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
